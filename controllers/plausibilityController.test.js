f4788fb6cfd948eb575f48e79558fbc6100832ad
Poslovitch <poslovitch@protonmail.com>
2025-07-15 14:31:57 +0200

    test: fix tests broken by the change from req body to query params

--- controllers/plausibilityController.test.js
+++ controllers/plausibilityController.test.js
@@ -6,10 +6,10 @@ const { getUserById } = require("./userController");
 jest.mock("../models");
 jest.mock("./userController");
 
-const mockRequest = (body, sessionData) => {
+const mockRequest = (query, sessionData) => {
   return {
     session: { data: sessionData },
-    body: body,
+    query: query ?? {},
   };
 };
 
@@ -30,7 +30,7 @@ describe("getText", () => {
     "should return 404 if there is no $textType texts available",
     async ({ randomValue, expectedErrorCode }) => {
       mockRandom(randomValue); // force going into the proper probability branch, but not great that the test "knows" how it works inside...
-      const req = mockRequest({ userId: 1 });
+      const req = mockRequest({ user: 1 });
       const res = mockResponse();
       getUserById.mockReturnValue({ id: 1 });
       await getText(req, res);
@@ -42,7 +42,7 @@ describe("getText", () => {
 
   it("should return 404 if group text has no associated text", async () => {
     mockRandom(0.3); // force going to the proper branch
-    const req = mockRequest({ userId: 1 });
+    const req = mockRequest({ user: 1 });
     const res = mockResponse();
     getUserById.mockReturnValue({ id: 1 });
 
@@ -58,7 +58,7 @@ describe("getText", () => {
 
   it("should return 200 with text data if test text is available", async () => {
     mockRandom(0.0);
-    const req = mockRequest({ userId: 1 });
+    const req = mockRequest({ user: 1 });
     const res = mockResponse();
     getUserById.mockReturnValue({ id: 1 });
 
@@ -75,7 +75,7 @@ describe("getText", () => {
     expect(res.json).toHaveBeenCalledWith(expected);
   });
 
-  it("should return 400 if no userId was provided in req body", async () => {
+  it("should return 400 if no user was provided in req query params", async () => {
     const req = mockRequest();
     const res = mockResponse();
     getUserById.mockReturnValue(undefined);
