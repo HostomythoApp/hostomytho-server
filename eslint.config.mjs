06a147540a94d4b2a8f5d02c8bb6e80924382969
Poslovitch <poslovitch@protonmail.com>
2025-07-15 09:48:32 +0200

    build: improve lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,12 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
+import jest from 'eslint-plugin-jest'
 
 export default defineConfig([
+  globalIgnores(['**/coverage/*', '**/hostomythoenv/*']),
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.test.{js,mjs,cjs}"], plugins: {jest}, extends: ['jest/recommended'], languageOptions: {globals: globals.jest}},
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
 ]);
