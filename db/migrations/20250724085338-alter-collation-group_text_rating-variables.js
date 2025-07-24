a27d73ebb0efb8b065efaffa2f1b931cbab5c3f4
Poslovitch <poslovitch@protonmail.com>
2025-07-24 11:26:45 +0200

    style: apply format

--- db/migrations/20250724085338-alter-collation-group_text_rating-variables.js
+++ db/migrations/20250724085338-alter-collation-group_text_rating-variables.js
@@ -3,12 +3,16 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, _Sequelize) {
-    await queryInterface.sequelize.query("ALTER TABLE group_text_rating COLLATE utf8mb4_unicode_ci;");
+    await queryInterface.sequelize.query(
+      "ALTER TABLE group_text_rating COLLATE utf8mb4_unicode_ci;"
+    );
     await queryInterface.sequelize.query("ALTER TABLE variables COLLATE utf8mb4_unicode_ci;");
   },
 
   async down(queryInterface, _Sequelize) {
-    await queryInterface.sequelize.query("ALTER TABLE group_text_rating COLLATE utf8mb4_general_ci;");
+    await queryInterface.sequelize.query(
+      "ALTER TABLE group_text_rating COLLATE utf8mb4_general_ci;"
+    );
     await queryInterface.sequelize.query("ALTER TABLE variables COLLATE utf8mb4_general_ci;");
   },
 };
